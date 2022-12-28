5199b3570f71ba7f350125c875e291b50989b2a9
Cezar Halmagean <cezar@mixandgo.com>
2022-12-28 10:04:06 +0200

    Attaching an avatar to User

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to users_path
+    else
+      render :new, :unprocessable_entity
+    end
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :avatar)
+    end
+end
