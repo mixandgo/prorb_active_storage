6641cee2e0e14e9b918c5ca9b8e321a6bd7d751d
Cezar Halmagean <cezar@mixandgo.com>
2022-12-30 12:34:18 +0200

    Defined named variant

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
-  has_one_attached :avatar
+  has_one_attached :avatar do |att|
+    att.variant :thumb, resize_to_limit: [100, 100]
+  end
   has_many_attached :files
 end
