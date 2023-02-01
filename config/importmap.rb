5433cbdb430cc3a6f0e4a93c5357b2346b74bc3d
Cezar Halmagean <cezar@mixandgo.com>
2023-02-01 12:59:09 +0200

    Direct upload

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@rails/activestorage", to: "https://ga.jspm.io/npm:@rails/activestorage@7.0.4-1/app/assets/javascripts/activestorage.esm.js"
+pin "direct_upload", preload: true
